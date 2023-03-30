a0acabeb58ee0c759fd92a76e0a5af81d6ebb8ca
Rong Mantle Bao <baorong2005@126.com>
2023-03-30 13:14:40 +0800

    docs: Change code license to GPLv3

--- firmware/lib/fwlib/fw_uart.c
+++ firmware/lib/fwlib/fw_uart.c
@@ -1,5 +1,4 @@
 // Copyright 2021 IOsetting <iosetting(at)outlook.com>
-// Copyright 2022-2023 Rong Bao (CSharperMantle) <baorong2005@126.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
